20259982afc32551a73765eeb378abb33e70d45d
Jacques Le Roux <jleroux@apache.org>
2007-06-05 11:29:26 +0000

    Applied fix from trunk for revision: 543408
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches@544448 13f79535-47bb-0310-9956-ffa450edef68

--- release4.0/mergefromtrunk.bat
+++ release4.0/mergefromtrunk.bat
@@ -1,5 +1,8 @@
+echo off
 rem DOS version of mergefromtrunk.sh. A bit clumsy but it works. If someone is able to do better I am taker JLR 2007/05/27
+rem to use : mergefromtrunk trunk_rev_number_to_merge_in _release
 set /a prevRev=%1% - 1
 rem DOS eats char below %1%. It eats exactly "https:" don't know why, I tried to add them : it works (DOS ways are impenetrable)
+echo on
 svn merge -r %prevRev%:%1% https: https://svn.apache.org/repos/asf/ofbiz/trunk
 svn commit -m "Applied fix from trunk for revision: %1%"
