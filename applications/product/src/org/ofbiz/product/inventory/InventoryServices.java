7d68f802e96839bd5e7ec95cbc29581a81bf9894
Scott Gray <lektran@apache.org>
2007-06-25 09:05:34 +0000

    Applied fix from trunk for revision: 549024
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release4.0@550421 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/src/org/ofbiz/product/inventory/InventoryServices.java
+++ applications/product/src/org/ofbiz/product/inventory/InventoryServices.java
@@ -918,7 +918,7 @@ public class InventoryServices {
                 salesUsageIt = delegator.findListIteratorByCondition(salesUsageViewEntity, 
                         new EntityConditionList(
                             UtilMisc.toList(
-                                new EntityExpr("facilityId", EntityOperator.EQUALS, "WebStoreWarehouse"),
+                                new EntityExpr("facilityId", EntityOperator.EQUALS, facilityId),
                                 new EntityExpr("productId", EntityOperator.EQUALS, productId),
                                 new EntityExpr("statusId", EntityOperator.IN, UtilMisc.toList("ORDER_COMPLETED", "ORDER_APPROVED", "ORDER_HELD")),
                                 new EntityExpr("orderTypeId", EntityOperator.EQUALS, "SALES_ORDER"),
@@ -956,7 +956,7 @@ public class InventoryServices {
                 productionUsageIt = delegator.findListIteratorByCondition(productionUsageViewEntity, 
                         new EntityConditionList(
                             UtilMisc.toList(
-                                new EntityExpr("facilityId", EntityOperator.EQUALS, "WebStoreWarehouse"),
+                                new EntityExpr("facilityId", EntityOperator.EQUALS, facilityId),
                                 new EntityExpr("productId", EntityOperator.EQUALS, productId),
                                 new EntityExpr("workEffortTypeId", EntityOperator.EQUALS, "PROD_ORDER_TASK"),
                                 new EntityExpr("actualCompletionDate", EntityOperator.GREATER_THAN_EQUAL_TO, checkTime)
