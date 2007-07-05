ce787644669ad87f15d1aead3cace207b8519ace
Scott Gray <lektran@apache.org>
2007-07-05 09:42:48 +0000

    Applied fix from trunk for revision: 543719
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release4.0@553439 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/src/org/ofbiz/accounting/payment/PaymentGatewayServices.java
+++ applications/accounting/src/org/ofbiz/accounting/payment/PaymentGatewayServices.java
@@ -1164,9 +1164,7 @@ public class PaymentGatewayServices {
                     amountCapturedBd = amountCapturedBd.setScale(2, BigDecimal.ROUND_HALF_UP);
 
                     // decrease amount of next payment preference to capture
-                    if (amountCaptured != null) {
-                        amountToCapture = amountToCapture.subtract(amountCapturedBd);                
-                    }
+                    amountToCapture = amountToCapture.subtract(amountCapturedBd);                
 
                     // add the invoiceId to the result for processing
                     captureResult.put("invoiceId", invoiceId);
