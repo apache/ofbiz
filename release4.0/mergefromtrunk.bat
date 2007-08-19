945b04e95de3ff6f32f4c99edebfc8cb6aaa1d5e
Jacques Le Roux <jleroux@apache.org>
2007-08-19 17:26:09 +0000

    Update : interactive DOS version of mergefromtrunk.sh.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches@567434 13f79535-47bb-0310-9956-ffa450edef68

--- release4.0/mergefromtrunk.bat
+++ release4.0/mergefromtrunk.bat
@@ -1,8 +1,10 @@
+rem interactive DOS version of mergefromtrunk.sh.
+rem to use : launch and pass the trunk version number to merge in release
+
 echo off
-rem DOS version of mergefromtrunk.sh. A bit clumsy but it works. If someone is able to do better I am taker JLR 2007/05/27
-rem to use : mergefromtrunk trunk_rev_number_to_merge_in _release
-set /a prevRev=%1% - 1
-rem DOS eats char below %1%. It eats exactly "https:" don't know why, I tried to add them : it works (DOS ways are impenetrable)
+set /p version=version to merge :
+set /a prevRev=%version% - 1
 echo on
-svn merge -r %prevRev%:%1% https: https://svn.apache.org/repos/asf/ofbiz/trunk
-svn commit -m "Applied fix from trunk for revision: %1%"
+svn merge -r %prevRev%:%version% https://svn.apache.org/repos/asf/ofbiz/trunk
+svn commit -m "Applied fix from trunk for revision: %version%"
+pause
