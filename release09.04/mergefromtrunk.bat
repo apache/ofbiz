9e1a14e4a0cdb58f32790d86331343379dc13972
Jacques Le Roux <jleroux@apache.org>
2009-04-29 07:02:39 +0000

    Applied fix from trunk for revision: 768960 \n
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches@769688 13f79535-47bb-0310-9956-ffa450edef68

--- release09.04/mergefromtrunk.bat
+++ release09.04/mergefromtrunk.bat
@@ -6,7 +6,10 @@ rem since we have now svn:mergeinfo changing root ("."), we need to update befor
 svn up
 set /p version=version to merge :
 set /a prevRev=%version% - 1
+svn log -r %version% > log.tmp
+set /p log = < log.tmp 
 echo on
 svn merge -r %prevRev%:%version% https://svn.apache.org/repos/asf/ofbiz/trunk
-svn commit -m "Applied fix from trunk for revision: %version%"
+svn commit -m "Applied fix from trunk for revision: %version% \n %log%"
 pause
+del log.tmp
