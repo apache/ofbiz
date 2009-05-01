100063e9cd180a8c9ade952da8dc4aeef6bf7dd3
Jacques Le Roux <jleroux@apache.org>
2009-05-01 17:03:53 +0000

    A patch from Ashish Vijaywargiya "Improved functionality of mergefromtrunk.sh" (https://issues.apache.org/jira/browse/OFBIZ-2404) - OFBIZ-2404
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release09.04@770755 13f79535-47bb-0310-9956-ffa450edef68

--- mergefromtrunk.sh
+++ mergefromtrunk.sh
@@ -2,5 +2,11 @@
 
 prevRev=`expr $1 - 1`
 svn merge -r $prevRev:$1 https://svn.apache.org/repos/asf/ofbiz/trunk 
-svn commit -m "Applied fix from trunk for revision: $1"
+trunkLog=runtime/trunkLog.xml
+touch ${trunkLog}
+svn log --xml -r $1> ${trunkLog}
+releaseBranchMessage="Applied fix from trunk for revision: $1 \n"
+trunkMessage=`grep -e '<msg>' ${trunkLog} | sed 's/<msg>//' | sed 's/<\/msg>//'` 
+rm -rf ${trunkLog}
+svn commit -m "`echo ${releaseBranchMessage} ${trunkMessage}`"
 
