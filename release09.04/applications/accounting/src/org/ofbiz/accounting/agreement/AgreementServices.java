80065fb1839755018bca494209c7303aec12d8b1
Ashish Vijaywargiya <ashish@apache.org>
2009-07-06 11:37:40 +0000

    Applied fix from trunk revision: 791445 Applied patch from jira issue OFBIZ-2705 (Commission Invoice doesn't show correct price and quantity) Thanks Amit for your contribution on this. I have extensively reviewed this work and it seems to be working fine.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches@791446 13f79535-47bb-0310-9956-ffa450edef68

--- release09.04/applications/accounting/src/org/ofbiz/accounting/agreement/AgreementServices.java
+++ release09.04/applications/accounting/src/org/ofbiz/accounting/agreement/AgreementServices.java
@@ -134,14 +134,14 @@ public class AgreementServices {
                         BigDecimal termValue = elem.getBigDecimal("termValue");
                         if (termValue != null) {
                             if (termTypeId.equals("FIN_COMM_FIXED")) {
-                                commission = commission.add(termValue.multiply(quantity));
+                                commission = commission.add(termValue);
                             } else if (termTypeId.equals("FIN_COMM_VARIABLE")) {
                                 // if variable percentage commission, need to divide by 100, because 5% is stored as termValue of 5.0
                                 commission = commission.add(termValue.multiply(amount).divide(new BigDecimal("100"), 12, rounding));
                             } else if (termTypeId.equals("FIN_COMM_MIN")) {
-                                min = termValue.multiply(quantity);
+                                min = termValue;
                             } else if (termTypeId.equals("FIN_COMM_MAX")) {
-                                max = termValue.multiply(quantity);
+                                max = termValue;
                             }
                             // TODO: Add other type of terms and handling here
                         }
@@ -169,6 +169,7 @@ public class AgreementServices {
                             "partyIdFrom", agreementItem.getString("partyIdFrom"),
                             "partyIdTo", agreementItem.getString("partyIdTo"),
                             "commission", commission,
+                            "quantity", quantity,
                             "currencyUomId", agreementItem.getString("currencyUomId"),
                             "productId", productId);
                     if (days >= 0) {
