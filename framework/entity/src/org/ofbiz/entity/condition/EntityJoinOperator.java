91340df391bb3a6da0d6dc76171c67daa40219b2
Adam Heath <doogie@apache.org>
2009-09-14 17:03:52 +0000

    Applied fix from trunk for revision: 814731  Fix AND and OR support when evaluating some conditions in memory;
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release09.04@814732 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/condition/EntityJoinOperator.java
+++ framework/entity/src/org/ofbiz/entity/condition/EntityJoinOperator.java
@@ -113,8 +113,8 @@ public class EntityJoinOperator extends EntityOperator<Boolean> {
     }
 
     public boolean entityMatches(GenericEntity entity, EntityCondition lhs, EntityCondition rhs) {
-        if (lhs.entityMatches(entity)) return shortCircuitValue;
-        if (rhs.entityMatches(entity)) return shortCircuitValue;
+        if (lhs.entityMatches(entity) == shortCircuitValue) return shortCircuitValue;
+        if (rhs.entityMatches(entity) == shortCircuitValue) return shortCircuitValue;
         return !shortCircuitValue;
     }
 
@@ -127,8 +127,8 @@ public class EntityJoinOperator extends EntityOperator<Boolean> {
     }
 
     public boolean mapMatches(GenericDelegator delegator, Map<String, ? extends Object> map, Object lhs, Object rhs) {
-        if (((EntityCondition) lhs).mapMatches(delegator, map)) return shortCircuitValue;
-        if (((EntityCondition) rhs).mapMatches(delegator, map)) return shortCircuitValue;
+        if (((EntityCondition) lhs).mapMatches(delegator, map) == shortCircuitValue) return shortCircuitValue;
+        if (((EntityCondition) rhs).mapMatches(delegator, map) == shortCircuitValue) return shortCircuitValue;
         return !shortCircuitValue;
     }
 
