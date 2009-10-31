a90e329c1d75fcccd5b366942068ceaa33e023f7
Hans Bakker <hansbak@apache.org>
2009-10-31 01:22:47 +0000

    add birt now in framework, and show as a secondary application
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/addbirt@831503 13f79535-47bb-0310-9956-ffa450edef68

---
+++ framework/birt/src/org/ofbiz/birt/report/context/OFBizBirtContext.java
@@ -0,0 +1,29 @@
+package org.ofbiz.birt.report.context;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.birt.report.IBirtConstants;
+import org.eclipse.birt.report.context.BirtContext;
+import org.eclipse.birt.report.context.ViewerAttributeBean;
+
+public class OFBizBirtContext extends BirtContext {
+
+    public OFBizBirtContext(HttpServletRequest request,
+            HttpServletResponse response) {
+        super(request, response);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void __init() {
+        // TODO Auto-generated method stub
+        this.bean = (ViewerAttributeBean) request
+                .getAttribute( IBirtConstants.ATTRIBUTE_BEAN );
+        if ( bean == null )
+        {
+            bean = new BirtViewerAttributeBean( request );
+        }
+        request.setAttribute( IBirtConstants.ATTRIBUTE_BEAN, bean );
+    }
+}
