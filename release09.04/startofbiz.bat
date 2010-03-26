208ba4b2244579c1eb460ef483b589e8d82516e1
Jacques Le Roux <jleroux@apache.org>
2010-03-26 14:39:45 +0000

    "Applied fix from trunk for revision: 927870  " ------------------------------------------------------------------------ r927870 | jleroux | 2010-03-26 15:34:02 +0100 (ven. 26 mars 2010) | 2 lignes
    Fix a NPE I got on actualCurrencyAmount while working with R9.04. 
    Since the files are identical I commit in trunk and will backport
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches@927876 13f79535-47bb-0310-9956-ffa450edef68

--- release09.04/startofbiz.bat
+++ release09.04/startofbiz.bat
@@ -51,5 +51,5 @@ rem "%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -Xdebug -Xnoagent -Djava.compiler=N
 
 rem ### Simple easy to read line
 echo on
-"%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -jar ofbiz.jar > runtime\logs\console.log
+"%JAVA_HOME%\bin\java" -Xms128M -Xmx512M -jar ofbiz.jar 
 
