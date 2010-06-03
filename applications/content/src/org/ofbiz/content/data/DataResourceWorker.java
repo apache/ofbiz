3b292c78ab50ffcac3a03fd02215d4dc2df37ca0
Jacques Le Roux <jleroux@apache.org>
2010-06-03 04:22:21 +0000

    "Applied fix from trunk for revision: 950866  " (with some handwork: conflict in data/DataResourceWorker.java) ------------------------------------------------------------------------ r950866 | jleroux | 2010-06-03 06:11:32 +0200 (jeu. 03 juin 2010) | 3 lignes
    A patch from Sumit Pandit "Issue in rendering Freemarker Template from String." (https://issues.apache.org/jira/browse/OFBIZ-3782) - OFBIZ-3782
    
    With the help of Bob Morley (it was more a cache issue than what Sumit thought)
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release09.04@950868 13f79535-47bb-0310-9956-ffa450edef68

--- applications/content/src/org/ofbiz/content/data/DataResourceWorker.java
+++ applications/content/src/org/ofbiz/content/data/DataResourceWorker.java
@@ -578,6 +578,20 @@ public class DataResourceWorker  implements org.ofbiz.widget.DataResourceWorkerI
     // -------------------------------------
 
     public static String renderDataResourceAsText(GenericDelegator delegator, String dataResourceId, Map templateContext,
+    public static void clearAssociatedRenderCache(Delegator delegator, String dataResourceId) throws GeneralException {
+        if (dataResourceId == null) {
+            throw new GeneralException("Cannot clear dataResource related cache for a null dataResourceId");
+        }
+
+        GenericValue dataResource = delegator.findByPrimaryKeyCache("DataResource", UtilMisc.toMap("dataResourceId", dataResourceId));
+        if (dataResource != null) {
+            String dataTemplateTypeId = dataResource.getString("dataTemplateTypeId");
+            if ("FTL".equals(dataTemplateTypeId)) {
+                FreeMarkerWorker.clearTemplateFromCache("DataResource:" + dataResourceId);        
+            }
+        }
+    }
+
              Locale locale, String targetMimeTypeId, boolean cache) throws GeneralException, IOException {
         Writer writer = new StringWriter();
         renderDataResourceAsText(delegator, dataResourceId, writer, templateContext, locale, targetMimeTypeId, cache);
