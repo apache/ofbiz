69be402e444b164c28c81df0f11cf2204765b9f4
Jacques Le Roux <jleroux@apache.org>
2010-07-31 13:12:22 +0000

    "Applied fix from trunk for revision: 981051" ------------------------------------------------------------------------ r981051 | jleroux | 2010-07-31 15:07:13 +0200 (sam. 31 juil. 2010) | 12 lignes
    A patch from Rene Scheibe "Click Expand or Colaspe All does not work as expected" (https://issues.apache.org/jira/browse/OFBIZ-3744) - OFBIZ-3744
    
    Reported by Sam Hamilton:
    If you are on say this page - https://localhost:8443/catalog/control/EditProduct?productId=WG-9943
    clicking on Expand All it will expand all the ones not expanded and then collapse the ones that were already open
    clicking on Collapse All it will collapse all the ones open and open all the ones that were collapsed.
    
    fixed issue
    
        * css.style.visible cannot be true/false as used so in collapseAll()
        * now using the $() container and calling visible() which returns true/false
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release10.04@981053 13f79535-47bb-0310-9956-ffa450edef68

--- framework/images/webapp/images/selectall.js
+++ framework/images/webapp/images/selectall.js
@@ -523,7 +523,7 @@ function expandAll(expanded) {
             groupbody=divs1[j];
           }
         }
-        if(groupbody.style.visible != expanded) {
+        if($(groupbody).visible() != expanded) {
           toggleCollapsiblePanel(links[0], groupbody.id, 'expand', 'collapse');
         }
       }
