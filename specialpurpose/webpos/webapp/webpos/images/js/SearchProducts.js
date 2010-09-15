10c1c19927af87cebd6f0da5aad92ef429cb288d
Adam Heath <doogie@apache.org>
2010-09-15 18:19:50 +0000

    Applied fix from trunk for revision: 997420 ===
    Some places in ofbiz were doing dom manipulation without waiting for the
    dom to be finished loading.  Wrap such code with a dom:loaded observer.
    
    Also, change all window->load calls to dom:loaded observers.  The former
    happens after all images have finished loading, while the later happens
    just after the dom has finished parsing.  This is better, as sometimes 
    the images may take a long time to finish downloading, and until they do,
    none of the manipulation will take place.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release09.04@997439 13f79535-47bb-0310-9956-ffa450edef68

--- specialpurpose/webpos/webapp/webpos/images/js/SearchProducts.js
+++ specialpurpose/webpos/webapp/webpos/images/js/SearchProducts.js
@@ -16,7 +16,7 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 */
-Event.observe(window, 'load', function() {
+document.observe('dom:loaded', function() {
 
     // Autocompleter for good identification field
     var j = 0;
