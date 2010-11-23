2c33b1f5b92eb6f91a06e184a303a955b29a83a9
Jacques Le Roux <jleroux@apache.org>
2010-11-23 09:48:14 +0000

    Revert r1038023, wrong repo (thanks to Erwan for reporting me)
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/jquery@1038027 13f79535-47bb-0310-9956-ffa450edef68

--- createFunambolDB.bat
+++
@@ -1,4 +0,0 @@
-rem To quickly populate the Funambol DB (DB must be created before)
-ant -f opentaps/funambol/install/install.xml install
-ant -f opentaps/funambol/install/install.xml install-sync-module
-ant -f opentaps/funambol/install/install.xml install-sync-source
