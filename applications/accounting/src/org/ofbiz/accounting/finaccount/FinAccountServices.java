f1d9bed7c96b053dddcbf17e0c913d49790a9373
Jacques Le Roux <jleroux@apache.org>
2011-02-11 22:17:43 +0000

    "Applied fix from trunk for revision: 1069965" ------------------------------------------------------------------------ r1069965 | jleroux | 2011-02-11 23:07:22 +0100 (ven., 11 févr. 2011) | 4 lines
    A patch from Chirag Manocha "createFinAccountForStore service should create FinAccount with currencyUomId of Store rather than default currency in general.properties." (https://issues.apache.org/jira/browse/OFBIZ-4163) - OFBIZ-4163
    
    The service createFinAccountForStore is creating FinAccount record with default currency given in general.properties.
    It should create FinAccount with the defaultCurrencyUomId of store.
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release10.04@1069970 13f79535-47bb-0310-9956-ffa450edef68

--- applications/accounting/src/org/ofbiz/accounting/finaccount/FinAccountServices.java
+++ applications/accounting/src/org/ofbiz/accounting/finaccount/FinAccountServices.java
@@ -179,6 +179,7 @@ public class FinAccountServices {
         GenericValue userLogin = (GenericValue) context.get("userLogin");
         String productStoreId = (String) context.get("productStoreId");
         String finAccountTypeId = (String) context.get("finAccountTypeId");
+        GenericValue productStore = ProductStoreWorker.getProductStore(productStoreId, delegator);
 
         try {
             // get the product store id and use it to generate a unique fin account code
@@ -217,6 +218,7 @@ public class FinAccountServices {
             // product store payToPartyId
             String payToPartyId = ProductStoreWorker.getProductStorePayToPartyId(productStoreId, delegator);
             inContext.put("organizationPartyId", payToPartyId);
+            inContext.put("currencyUomId", productStore.get("defaultCurrencyUomId"));
 
             Map<String, Object> createResult = dispatcher.runSync("createFinAccount", inContext);
 
