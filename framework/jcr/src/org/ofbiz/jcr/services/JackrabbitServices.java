75eea6dc85f2354c27defe3504aa81579083f2e8
Sascha Rodekamp <sascharodekamp@apache.org>
2011-09-29 12:00:32 +0000

    Add versionning for to the speedtest
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/jackrabbit20100709@1177274 13f79535-47bb-0310-9956-ffa450edef68

--- framework/jcr/src/org/ofbiz/jcr/services/JackrabbitServices.java
+++ framework/jcr/src/org/ofbiz/jcr/services/JackrabbitServices.java
@@ -3,7 +3,11 @@ package org.ofbiz.jcr.services;
 import java.util.Date;
 import java.util.Map;
 
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.version.VersionManager;
 
 import org.ofbiz.base.util.Debug;
 import org.ofbiz.entity.GenericValue;
@@ -17,7 +21,7 @@ public class JackrabbitServices {
 
     private static String module = JackrabbitServices.class.getName();
 
-    public static Map<String, Object> determineJackrabbitRepositorySpeed(DispatchContext ctx, Map<String, Object> context) {
+    public static Map<String, Object> determineJackrabbitRepositorySpeed(DispatchContext ctx, Map<String, Object> context) throws UnsupportedRepositoryOperationException, RepositoryException {
         Map<String, Object> result = ServiceUtil.returnSuccess();
         GenericValue userLogin = (GenericValue) context.get("userLogin");
         Integer maxNodes = (Integer) context.get("maxNodes");
@@ -26,12 +30,18 @@ public class JackrabbitServices {
         Long diff = 0l;
 
         Session session = JCRFactoryUtil.getSession();
+        VersionManager vm = session.getWorkspace().getVersionManager();
         start = new Date().getTime();
         for (int i = 0; i <= maxNodes; i++) {
             try {
                 // add a node
-                session.getRootNode().addNode("__Speedtest_Node-" + i);
+                Node n = session.getRootNode().addNode("__Speedtest_Node-" + i);
+                n.addMixin("mix:versionable");
+                n.setProperty("anyProperty", "Blah");
                 session.save();
+                vm.checkin(n.getPath());
+
+                vm.checkout("/__Speedtest_Node-" + i);
                 // remove the node
                 session.removeItem("/__Speedtest_Node-" + i);
                 session.save();
