b78d3e9fce0f0a537fe050bea9918790ed612712
Jacques Le Roux <jleroux@apache.org>
2011-12-31 10:16:19 +0000

    "Applied fix from trunk for revision: 1226055  " ------------------------------------------------------------------------ r1226055 | jleroux | 2011-12-31 11:13:34 +0100 (sam., 31 déc. 2011) | 9 lines
    Should close "Null values are not synchronized in http mode" https://issues.apache.org/jira/browse/OFBIZ-4602
    
    I followed Adrian's advice. I tried to create an order with a dropship1 item (like in Jira 4637) and it worked perfectly. I guess, as we were suspecting, using 
      set(name, "");
    instead of
      element.setAttribute(name, "null");
    had side effects. 
    
    Anyway, as Adrian pointed out, the right returned string should be "[null-field]" and it's now correct/consistent. On the other side (entity sync clients), of course, now "[null-field]" should be handled instead of "" or "null"
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release09.04@1226057 13f79535-47bb-0310-9956-ffa450edef68

--- framework/entity/src/org/ofbiz/entity/GenericEntity.java
+++ framework/entity/src/org/ofbiz/entity/GenericEntity.java
@@ -987,6 +987,8 @@ public class GenericEntity extends Observable implements Map<String, Object>, Lo
                 } else {
                     element.setAttribute(name, value);
                 }
+            } else {
+                element.setAttribute(name, GenericEntity.NULL_FIELD.toString());
             }
         }
 
