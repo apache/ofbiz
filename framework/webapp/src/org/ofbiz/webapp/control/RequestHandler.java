88f218c52a84c936ae0a2d405669faa5bbb64706
Sascha Rodekamp <sascharodekamp@apache.org>
2012-03-05 12:34:50 +0000

    No Url encoding for get parameters (https://issues.apache.org/jira/browse/OFBIZ-2628) using the URLEncoder to encode and render URLs with special Chars. The encoding is always UTF-8
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release10.04@1297030 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/control/RequestHandler.java
+++ framework/webapp/src/org/ofbiz/webapp/control/RequestHandler.java
@@ -24,6 +24,8 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.Locale;
@@ -1079,7 +1081,12 @@ public class RequestHandler {
                         newURL.insert(questionIndex, sessionId);
                     }
                 }
-                encodedUrl = newURL.toString();
+                try {
+                    encodedUrl = URLEncoder.encode(newURL.toString(), Charset.forName("UTF-8").displayName());
+                } catch (UnsupportedEncodingException e) {
+                    Debug.logError(e, module);
+                    encodedUrl = newURL.toString();
+                }
             }
         } else {
             encodedUrl = newURL.toString();
