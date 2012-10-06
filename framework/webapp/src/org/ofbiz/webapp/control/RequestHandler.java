5a02c3be56c9d72470e42932503403f17b562074
Jacques Le Roux <jleroux@apache.org>
2012-10-06 12:53:55 +0000

    "Applied fix from trunk for revision: 1394995" ------------------------------------------------------------------------ r1394995 | jleroux | 2012-10-06 12:59:12 +0200 (sam., 06 oct. 2012) | 9 lines
    A modified patch from  Jyoti Sharma https://issues.apache.org/jira/browse/OFBIZ-5043
    
    When i worked with Url Rewrite Filter, i faced the following issue:-
    In case of http protocol in the url, https links on th epage were not being rewritten and when their was https url, http links were not rewritten.
    
    The issue behind this what i found is that in RequestHandler.java encodeUrl() is not called in the above given case.
    So, if we place response.encodeUrl() their the issue is resolved. 
    
    jleroux: This makes sense since we are still in the "if (encode) {" block. I just noticed that response != null was checked above so I placed the same check before encoding
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release11.04@1395036 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/control/RequestHandler.java
+++ framework/webapp/src/org/ofbiz/webapp/control/RequestHandler.java
@@ -1112,7 +1112,11 @@ public class RequestHandler {
                         newURL.insert(questionIndex, sessionId);
                     }
                 }
-                encodedUrl = newURL.toString();
+                if (response != null) {
+                    encodedUrl = response.encodeURL(newURL.toString());
+                } else {
+                    encodedUrl = newURL.toString();
+                }
             }
         } else {
             encodedUrl = newURL.toString();
