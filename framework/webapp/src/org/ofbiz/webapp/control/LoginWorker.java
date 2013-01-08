52f8b3b8b9759f749b82657a3c386fdfc94ec229
Jacques Le Roux <jleroux@apache.org>
2013-01-08 11:04:34 +0000

    Revert r1429273, there is a better solution that Deepak spotted
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release10.04@1430207 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/control/LoginWorker.java
+++ framework/webapp/src/org/ofbiz/webapp/control/LoginWorker.java
@@ -84,8 +84,6 @@ public class LoginWorker {
 
     /** This Map is keyed by the randomly generated externalLoginKey and the value is a UserLogin GenericValue object */
     public static Map<String, GenericValue> externalLoginKeys = FastMap.newInstance();
-    // This fast map is added as work around for repeated login issue
-    public static Map<String, GenericValue> externalLoginKeysWR = FastMap.newInstance();
 
     public static StringWrapper makeLoginUrl(PageContext pageContext) {
         return makeLoginUrl(pageContext, "checkLogin");
@@ -132,8 +130,6 @@ public class LoginWorker {
             // if the session has a previous key in place, remove it from the master list
             String sesExtKey = (String) session.getAttribute(EXTERNAL_LOGIN_KEY_ATTR);
             if (sesExtKey != null) {
-                // Work around for repeated login issue
-                externalLoginKeysWR.put(sesExtKey, userLogin);
                 externalLoginKeys.remove(sesExtKey);
             }
 
@@ -156,7 +152,6 @@ public class LoginWorker {
         String sesExtKey = (String) session.getAttribute(EXTERNAL_LOGIN_KEY_ATTR);
         if (sesExtKey != null) {
             externalLoginKeys.remove(sesExtKey);
-            externalLoginKeysWR.remove(sesExtKey);
         }
     }
 
@@ -527,7 +522,6 @@ public class LoginWorker {
     public static void doBasicLogin(GenericValue userLogin, HttpServletRequest request) {
         HttpSession session = request.getSession();
         session.setAttribute("userLogin", userLogin);
-        request.setAttribute("userLogin", userLogin);
 
         String javaScriptEnabled = null;
         try {
@@ -931,11 +925,6 @@ public class LoginWorker {
         if (externalKey == null) return "success";
 
         GenericValue userLogin = (GenericValue) LoginWorker.externalLoginKeys.get(externalKey);
-        // Work around for repeated login issue
-        if (userLogin == null) {
-            userLogin = (GenericValue) LoginWorker.externalLoginKeysWR.get(externalKey);
-            externalLoginKeysWR.remove(externalKey);
-        }
         if (userLogin != null) {
             //to check it's the right tenant
             //in case username and password are the same in different tenants
