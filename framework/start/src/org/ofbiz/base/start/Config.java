apache/ofbiz

commit 4fd4b3e5da3530147f8f02f39e0a7900dab0da71

Completes descriptions of ant targets which have portoffset parameter Completes code in Config.java for start-debug only (adds "start-debug" to pick start.properties when portoffset is used). The rest of the changes is only refactoring to have the replacement to pick start.properties only in getInstance()
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1574404 13f79535-47bb-0310-9956-ffa450edef68

build.xml
@@ -421,7 +421,7 @@ under the License.
         </java>
     </target>
     <target name="start-batch"
-            description="Start OFBiz as a separate process">
+            description="Start OFBiz as a separate process. Use -Dportoffset=portNumber to shift all ports with the portNumber value.">
         <java jar="ofbiz.jar" fork="true" spawn="true">
             <jvmarg value="${memory.initial.param}"/>
             <jvmarg value="${memory.max.param}"/>
@@ -431,7 +431,7 @@ under the License.
         </java>
     </target>
     <target name="start-debug"
-            description="Start OFBiz in debugging mode. It uses the 8091 port by default">
+            description="Start OFBiz in debugging mode. It uses the 8091 port by default. Use -Dportoffset=portNumber to shift all ports with the portNumber value.">
         <java jar="ofbiz.jar" fork="true">
             <jvmarg value="${memory.initial.param}"/>
             <jvmarg value="${memory.max.param}"/>
@@ -440,6 +440,7 @@ under the License.
             <jvmarg value="-Djava.compiler=NONE"/>
             <jvmarg value="-Xdebug"/>
             <jvmarg value="-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8091"/>
+            <arg value="start-debug"/>
             <arg value="-portoffset=${portoffset}"/>
         </java>
     </target>
@@ -464,14 +465,14 @@ under the License.
         </java>
     </target>
     <target name="stop"
-            description="Stop OFBiz">
+            description="Stop OFBiz. Use -Dportoffset=portNumber to shift all ports with the portNumber value.">
         <java jar="ofbiz.jar" fork="true">
             <arg value="-shutdown"/>
             <arg value="-portoffset=${portoffset}"/>
         </java>
     </target>
     <target name="status"
-            description="Display status of OFBiz">
+            description="Display status of OFBiz. Use -Dportoffset=portNumber to shift all ports with the portNumber value.">
         <java jar="ofbiz.jar" fork="true">
             <arg value="-status"/>
             <arg value="-portoffset=${portoffset}"/>

framework/start/src/org/ofbiz/base/start/Config.java
@@ -44,19 +44,17 @@ public class Config {
         if (command.startsWith("-")) {
             command = command.substring(1);
         }
-        // shutdown & status hack
-        if (command.equalsIgnoreCase("shutdown")) {
-            command = "start";
-        } else if (command.equalsIgnoreCase("status")) {
-            command = "start";
-        }
         return "org/ofbiz/base/start/" + command + ".properties";
     }
 
     public static Config getInstance(String[] args) throws IOException {
         String firstArg = args.length > 0 ? args[0] : "";
-        // Needed when portoffset is used with these commands
-        if ("start-batch".equals(firstArg) || "stop".equals(firstArg)) {
+        // Needed when portoffset is used with these commands, start.properties fits for all of them
+        if ("start-batch".equalsIgnoreCase(firstArg) 
+                || "start-debug".equalsIgnoreCase(firstArg) 
+                || "stop".equalsIgnoreCase(firstArg) 
+                || "shutdown".equalsIgnoreCase(firstArg) // shutdown & status hack (was pre-existing to portoffset introduction, also useful with it) 
+                || "status".equalsIgnoreCase(firstArg)) {
             firstArg = "start";
         }
         String configFileName = getConfigFileName(firstArg);

commit 0edfe98dd3a6a5584e0b65d01246b51320e2b969

"Applied fix from trunk for revision: 1577744" ------------------------------------------------------------------------ r1577744 | jleroux | 2014-03-14 23:20:34 +0100 (ven. 14 mars 2014) | 3 lignes
Fixes a bug introduced with r1574404 (forgot that shutdown and status historicaly have a prefixing dash), thanks to Youseek Khaye who spotted it and sent me a mail (the stop target was no longer working). I tested them only with a portoffset, my bad.

While working on this I spotted another error introduced earlier with r1533839. Now, status and stop work again as before. 
------------------------------------------------------------------------


git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release13.07@1577745 13f79535-47bb-0310-9956-ffa450edef68

build.xml
@@ -423,7 +423,7 @@ under the License.
             <jvmarg value="${memory.initial.param}"/>
             <jvmarg value="${memory.max.param}"/>
             <jvmarg value="${memory.maxpermsize.param}"/>
-        	  <arg value="start-batch"/>
+            <arg value="start-batch"/>
             <arg value="-portoffset=${portoffset}"/>
         </java>
     </target>

framework/start/src/org/ofbiz/base/start/Config.java
@@ -40,10 +40,6 @@ public class Config {
         if (command == null || command.trim().length() == 0) {
             command = "start";
         }
-        // strip off the leading dash
-        if (command.startsWith("-")) {
-            command = command.substring(1);
-        }
         return "org/ofbiz/base/start/" + command + ".properties";
     }
 
@@ -53,8 +49,8 @@ public class Config {
         if ("start-batch".equalsIgnoreCase(firstArg) 
                 || "start-debug".equalsIgnoreCase(firstArg) 
                 || "stop".equalsIgnoreCase(firstArg) 
-                || "shutdown".equalsIgnoreCase(firstArg) // shutdown & status hack (was pre-existing to portoffset introduction, also useful with it) 
-                || "status".equalsIgnoreCase(firstArg)) {
+                || "-shutdown".equalsIgnoreCase(firstArg) // shutdown & status hack (was pre-existing to portoffset introduction, also useful with it) 
+                || "-status".equalsIgnoreCase(firstArg)) {
             firstArg = "start";
         }
         String configFileName = getConfigFileName(firstArg);
@@ -356,14 +352,15 @@ public class Config {
             adminPort = Integer.parseInt(adminPortStr);
             if (args.length > 0) {
                 for (String arg : args) {
-                    if (arg.toLowerCase().contains("portoffset=")) {
+                    if (arg.toLowerCase().contains("portoffset=") && !arg.toLowerCase().contains("${portoffset}")) {
                         adminPort = adminPort != 0 ? adminPort : 10523; // This is necessary because the ASF machines don't allow ports 1 to 3, see  INFRA-6790
                         adminPort += Integer.parseInt(arg.split("=")[1]);
                     }
                 }
             }
         } catch (Exception e) {
-            adminPort = 0;
+            System.out.println("Error while parsing admin port number (so default to 10523) = " + e);
+            adminPort = 10523;
         }
 
         // set the Derby system home