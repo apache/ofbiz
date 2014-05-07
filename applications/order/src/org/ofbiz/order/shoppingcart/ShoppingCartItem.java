apache/ofbiz

commit c8526e50b70d72a8e1fdab68c1a583ae1262200e

Add an automatic product price currency conversion which can be switched off in a properties file.
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1125215 13f79535-47bb-0310-9956-ffa450edef68

applications/order/src/org/ofbiz/order/shoppingcart/ShoppingCartItem.java
@@ -1110,7 +1110,7 @@ public class ShoppingCartItem implements java.io.Serializable {
         if (_product != null && isModifiedPrice == false) {
             try {
                 Map<String, Object> priceContext = FastMap.newInstance();
-                priceContext.put("currencyUomId", cart.getCurrency());
+                priceContext.put("currencyUomIdTo", cart.getCurrency());
 
                 String partyId = cart.getPartyId();
                 if (partyId != null) {

applications/product/servicedef/services_pricepromo.xml
@@ -38,6 +38,7 @@ under the License.
         <attribute name="quantity" type="BigDecimal" mode="IN" optional="true"/>
         <attribute name="amount" type="BigDecimal" mode="IN" optional="true"/>
         <attribute name="currencyUomId" type="String" mode="IN" optional="true"/>
+        <attribute name="currencyUomIdTo" type="String" mode="IN" optional="true"/>
         <attribute name="productPricePurposeId" type="String" mode="IN" optional="true"><!-- defaults to PURCHASE --></attribute>
         <attribute name="termUomId" type="String" mode="IN" optional="true"><!-- if specified ProductPrice records will be filtered by this, ensures for purposes like recurring prices that only the recurring term desired is taken into consideration --></attribute>
         <attribute name="autoUserLogin" type="org.ofbiz.entity.GenericValue" mode="IN" optional="true"/>

specialpurpose/ecommerce/config/ecommerce.properties
@@ -1,0 +1,19 @@
+###############################################################################
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+###############################################################################
+convertProductPriceCurrency=true

commit 14a019de27756048d6b14057bbaa0e56a27fed2d

"Applied fix from trunk for revision: 1592977" ------------------------------------------------------------------------ r1592977 | jleroux | 2014-05-07 13:44:44 +0200 (mer. 07 mai 2014) | 4 lignes
Fixes a regression introduced by r1125215 "Add an automatic product price currency conversion which can be switched off in a properties file."
r1125215 broke the possibility to set a currency to a product store.
The automatic product price currency conversion should not be activated by default
Correctly formats changes done by r1125215 in PriceServices.java
------------------------------------------------------------------------


git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release13.07@1592978 13f79535-47bb-0310-9956-ffa450edef68

applications/order/src/org/ofbiz/order/shoppingcart/ShoppingCartItem.java
@@ -1198,7 +1198,11 @@ public class ShoppingCartItem implements java.io.Serializable {
                             }
                         }
                     }
-                    priceContext.put("currencyUomIdTo", cart.getCurrency());
+                    if ("true".equals(UtilProperties.getPropertyValue("catalog.properties", "convertProductPriceCurrency"))){
+                        priceContext.put("currencyUomIdTo", cart.getCurrency());
+                    } else {
+                        priceContext.put("currencyUomId", cart.getCurrency());
+                    }
                     priceContext.put("prodCatalogId", this.getProdCatalogId());
                     priceContext.put("webSiteId", cart.getWebSiteId());
                     priceContext.put("productStoreId", cart.getProductStoreId());

applications/product/config/catalog.properties
@@ -43,4 +43,4 @@ image.management.autoApproveImage=Y
 image.management.multipleApproval=N
 
 # Automatic product price currency conversion
-convertProductPriceCurrency=true
+convertProductPriceCurrency=false

applications/product/src/org/ofbiz/product/price/PriceServices.java
@@ -524,9 +524,9 @@ public class PriceServices {
         }
 
         // Convert the value to the price currency, if required
-        if("true".equals(UtilProperties.getPropertyValue("catalog.properties", "convertProductPriceCurrency"))){
+        if ("true".equals(UtilProperties.getPropertyValue("catalog.properties", "convertProductPriceCurrency"))) {
             if (UtilValidate.isNotEmpty(currencyDefaultUomId) && UtilValidate.isNotEmpty(currencyUomIdTo) && !currencyDefaultUomId.equals(currencyUomIdTo)) {
-                if(UtilValidate.isNotEmpty(result)){
+                if (UtilValidate.isNotEmpty(result)) {
                     Map<String, Object> convertPriceMap = FastMap.newInstance();
                     for(Map.Entry<String, Object> entry : result.entrySet()) {
                         BigDecimal tempPrice = BigDecimal.ZERO;
@@ -547,22 +547,23 @@ public class PriceServices {
                         else if (entry.getKey() == "listPrice")
                             tempPrice = (BigDecimal) entry.getValue();
                         
-                        if(tempPrice != null && tempPrice != BigDecimal.ZERO){
+                        if (tempPrice != null && tempPrice != BigDecimal.ZERO) {
                             Map<String, Object> priceResults = FastMap.newInstance();
                             try {
-                                priceResults = dispatcher.runSync("convertUom", UtilMisc.<String, Object>toMap("uomId", currencyDefaultUomId, "uomIdTo", currencyUomIdTo, "originalValue", tempPrice , "defaultDecimalScale" , Long.valueOf(2) , "defaultRoundingMode" , "HalfUp"));
+                                priceResults = dispatcher.runSync("convertUom", UtilMisc.<String, Object> toMap("uomId", currencyDefaultUomId, "uomIdTo", currencyUomIdTo,
+                                        "originalValue", tempPrice, "defaultDecimalScale", Long.valueOf(2), "defaultRoundingMode", "HalfUp"));
                                 if (ServiceUtil.isError(priceResults) || (priceResults.get("convertedValue") == null)) {
-                                    Debug.logWarning("Unable to convert " + entry.getKey() + " for product  " + productId , module);
-                                } 
+                                    Debug.logWarning("Unable to convert " + entry.getKey() + " for product  " + productId, module);
+                                }
                             } catch (GenericServiceException e) {
                                 Debug.logError(e, module);
                             }
                             convertPriceMap.put(entry.getKey(), priceResults.get("convertedValue"));
-                        }else{
+                        } else {
                             convertPriceMap.put(entry.getKey(), entry.getValue());
                         }
                     }
-                    if(UtilValidate.isNotEmpty(convertPriceMap)){
+                    if (UtilValidate.isNotEmpty(convertPriceMap)) {
                         convertPriceMap.put("currencyUsed", currencyUomIdTo);
                         result = convertPriceMap;
                     }