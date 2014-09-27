a318a6dec4eb3297d81e9fa953ccf38d38a4a1da
Anil Patel <apatel@apache.org>
2014-09-27 22:40:29 +0000

    [OFBIZ-3719] Reverting DOCTYPE hardcoding from cmsevents. Giving control back to the template
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release13.07@1628018 13f79535-47bb-0310-9956-ffa450edef68

--- applications/content/src/org/ofbiz/content/cms/CmsEvents.java
+++ applications/content/src/org/ofbiz/content/cms/CmsEvents.java
@@ -282,8 +282,6 @@ public class CmsEvents {
                     // TODO: replace "screen" to support dynamic rendering of different output
                     FormStringRenderer formStringRenderer = new MacroFormRenderer(UtilProperties.getPropertyValue("widget", "screen.formrenderer"), request, response);
                     templateMap.put("formStringRenderer", formStringRenderer);
-                    //include DOCTYPE for cms screens
-                    writer.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">");
                     
                     // if use web analytics
                     List<GenericValue> webAnalytics = delegator.findByAnd("WebAnalyticsConfig", UtilMisc.toMap("webSiteId", webSiteId), null, false);
