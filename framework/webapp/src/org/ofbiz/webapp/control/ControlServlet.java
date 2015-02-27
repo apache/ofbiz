393ac0268a37c5a6a4a4fe8a34ff829f2573a73c
Jacques Le Roux <jleroux@apache.org>
2015-02-27 21:47:01 +0000

    "Applied fix from trunk for revision: 1662812" Fixes "Birt doesn't work" reported by Leon at https://issues.apache.org/jira/browse/OFBIZ-5870
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release14.12@1662822 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webapp/src/org/ofbiz/webapp/control/ControlServlet.java
+++ framework/webapp/src/org/ofbiz/webapp/control/ControlServlet.java
@@ -37,6 +37,7 @@ import org.ofbiz.base.util.UtilGenerics;
 import org.ofbiz.base.util.UtilHttp;
 import org.ofbiz.base.util.UtilTimer;
 import org.ofbiz.base.util.UtilValidate;
+import org.ofbiz.base.util.template.FreeMarkerWorker;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.DelegatorFactory;
 import org.ofbiz.entity.GenericDelegator;
@@ -47,6 +48,8 @@ import org.ofbiz.security.Security;
 import org.ofbiz.service.LocalDispatcher;
 import org.ofbiz.webapp.stats.ServerHitBin;
 import org.ofbiz.webapp.stats.VisitHandler;
+
+import freemarker.ext.servlet.ServletContextHashModel;
 
 /**
  * ControlServlet.java - Master servlet for the web application.
@@ -186,6 +189,9 @@ public class ControlServlet extends HttpServlet {
 
         request.setAttribute("_REQUEST_HANDLER_", requestHandler);
         
+        ServletContextHashModel ftlServletContext = new ServletContextHashModel(this, FreeMarkerWorker.getDefaultOfbizWrapper());
+        request.setAttribute("ftlServletContext", ftlServletContext);
+
         // setup some things that should always be there
         UtilHttp.setInitialRequestInfo(request);
         VisitHandler.getVisitor(request, response);
