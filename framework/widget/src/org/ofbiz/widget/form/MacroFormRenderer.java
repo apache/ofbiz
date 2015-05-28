5b1f01c9f3de54bcd8de53ca05e7689db6a3f171
Nicolas Malin <nmalin@apache.org>
2015-05-28 21:42:25 +0000

    Apply patch from OFBIZ-6415 renderTextFindField doesn't reselect operation dropdown or ignoreCase checkbox by christian carlow
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release13.07@1682344 13f79535-47bb-0310-9956-ffa450edef68

--- framework/widget/src/org/ofbiz/widget/form/MacroFormRenderer.java
+++ framework/widget/src/org/ofbiz/widget/form/MacroFormRenderer.java
@@ -1676,7 +1676,7 @@ public class MacroFormRenderer implements FormStringRenderer {
 
     public void renderTextFindField(Appendable writer, Map<String, Object> context, TextFindField textFindField) throws IOException {
         ModelFormField modelFormField = textFindField.getModelFormField();
-        String defaultOption = textFindField.getDefaultOption();
+        String defaultOption = textFindField.getDefaultOption(context);
         String className = "";
         String alert = "false";
         String opEquals = "";
@@ -1717,7 +1717,7 @@ public class MacroFormRenderer implements FormStringRenderer {
             titleStyle = modelFormField.getTitleStyle();
         }
         String ignoreCase = UtilProperties.getMessage("conditional", "ignore_case", locale);
-        boolean ignCase = textFindField.getIgnoreCase();
+        boolean ignCase = textFindField.getIgnoreCase(context);
         boolean hideIgnoreCase = textFindField.getHideIgnoreCase();
         StringWriter sr = new StringWriter();
         sr.append("<@renderTextFindField ");
