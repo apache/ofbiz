755de8680b27224dc8c8845bbdb77164e70bea52
Jacques Le Roux <jleroux@apache.org>
2015-10-30 17:28:49 +0000

    "Applied fix from trunk for revision: 1711519" ------------------------------------------------------------------------ r1711519 | jleroux | 2015-10-30 18:26:25 +0100 (ven. 30 oct. 2015) | 1 ligne
    Fixes a typo after "clearchris"'s report on user ML.
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release14.12@1711521 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/src/org/ofbiz/product/config/ProductConfigItemContentWrapper.java
+++ applications/product/src/org/ofbiz/product/config/ProductConfigItemContentWrapper.java
@@ -119,6 +119,7 @@ public class ProductConfigItemContentWrapper implements ContentWrapper {
     public static String getProductConfigItemContentAsText(GenericValue productConfigItem, String confItemContentTypeId, Locale locale, String mimeTypeId, Delegator delegator, LocalDispatcher dispatcher, String encoderType) {
         UtilCodec.SimpleEncoder encoder = UtilCodec.getEncoder(encoderType);
         String candidateFieldName = ModelUtil.dbNameToVarName(confItemContentTypeId);
+        String cacheKey = confItemContentTypeId + SEPARATOR + locale + SEPARATOR + mimeTypeId + SEPARATOR + productConfigItem.get("configItemId");
         try {
             Writer outWriter = new StringWriter();
             getProductConfigItemContentAsText(null, productConfigItem, confItemContentTypeId, locale, mimeTypeId, delegator, dispatcher, outWriter);
