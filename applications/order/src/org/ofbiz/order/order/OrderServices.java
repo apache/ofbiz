eb65f559c0e839659c5055a7af4c60828f9b5db9
Deepak Dixit <deepak@apache.org>
2016-05-25 09:16:08 +0000

    (OFBIZ-7078)  Manually merged r#1744781 Log: ======================================================= Extend OrderAdjustment entity and add isManual field ======================================================= Thanks Rohit Koushal for your contribution.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release14.12@1745462 13f79535-47bb-0310-9956-ffa450edef68

--- applications/order/src/org/ofbiz/order/order/OrderServices.java
+++ applications/order/src/org/ofbiz/order/order/OrderServices.java
@@ -1640,7 +1640,7 @@ public class OrderServices {
         BigDecimal totalManuallyAddedOrderTax = ZERO;
         for (GenericValue orderTaxAdjustment : orderTaxAdjustments) {
             String comment = orderTaxAdjustment.getString("comments");
-            if (orderTaxAdjustment.get("amount") != null && comment !=null && comment.startsWith("Added manually by")) {
+            if (orderTaxAdjustment.get("amount") != null && "Y".equals(orderTaxAdjustment.getString("isManual"))) {
                 totalManuallyAddedOrderTax = totalManuallyAddedOrderTax.add(orderTaxAdjustment.getBigDecimal("amount").setScale(taxDecimals, taxRounding));
             }
         }
@@ -4325,7 +4325,7 @@ public class OrderServices {
                             // Removing objects from toStore list for old Shipping and Handling Charges Adjustment and Sales Tax Adjustment.
                             removeList.add(stored);
                         }
-                        if (stored.get("comments") != null && ((String)stored.get("comments")).startsWith("Added manually by")) {
+                        if ("Y".equals(stored.getString("isManual"))) {
                             // Removing objects from toStore list for Manually added Adjustment.
                             removeList.add(stored);
                         }
@@ -4335,7 +4335,7 @@ public class OrderServices {
             }
             for (GenericValue toAdd: toAddList) {
                 if ("OrderAdjustment".equals(toAdd.getEntityName())) {
-                    if (toAdd.get("comments") != null && ((String)toAdd.get("comments")).startsWith("Added manually by") && (("PROMOTION_ADJUSTMENT".equals(toAdd.get("orderAdjustmentTypeId"))) ||
+                    if ("Y".equals(toAdd.getString("isManual"))  && (("PROMOTION_ADJUSTMENT".equals(toAdd.get("orderAdjustmentTypeId"))) ||
                             ("SHIPPING_CHARGES".equals(toAdd.get("orderAdjustmentTypeId"))) || ("SALES_TAX".equals(toAdd.get("orderAdjustmentTypeId"))))) {
                         toStore.add(toAdd);
                     }
