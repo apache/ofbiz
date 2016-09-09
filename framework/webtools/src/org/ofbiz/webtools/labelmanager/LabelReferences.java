8b312c2c4d749904cf392e80d0012604b2b9802d
Jacques Le Roux <jleroux@apache.org>
2016-09-09 05:20:38 +0000

    "Applied fix from trunk for revision: 1759941" (tree conflict handled by hand) ------------------------------------------------------------------------ r1759941 | jleroux | 2016-09-09 07:06:51 +0200 (ven. 09 sept. 2016) | 6 lignes
    Fix for "LabelManager doesn't search .groovy files"
    OFBIZ-8153
    
    There are over 60 labels used in groovy files. These don't get reflected in the counts of the label manager and in the search for labels not used.
    
    Thanks to Pierre Smits for report
    ------------------------------------------------------------------------
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release13.07@1759942 13f79535-47bb-0310-9956-ffa450edef68

--- framework/webtools/src/org/ofbiz/webtools/labelmanager/LabelReferences.java
+++ framework/webtools/src/org/ofbiz/webtools/labelmanager/LabelReferences.java
@@ -108,8 +108,10 @@ public class LabelReferences {
         }
         // get labels from FTL files
         getLabelsFromFtlFiles();
-        // get labels from java files
+        // get labels from Java files
         getLabelsFromJavaFiles();
+        // get labels from Groovy files
+        getLabelsFromGroovyFiles();
         // get labels from simple method files
         getLabelsFromSimpleMethodFiles();
         // get labels from widgets files
@@ -203,6 +205,17 @@ public class LabelReferences {
             }
         }
     }
+
+    private void getLabelsFromGroovyFiles() throws IOException {
+        for (String rootFolder : this.rootFolders) {
+            List<File> groovyFiles = FileUtil.findFiles("groovy", rootFolder + "groovyScripts", null, null);
+            for (File file : groovyFiles) {
+                String inFile = FileUtil.readString("UTF-8", file);
+                findUiLabelMapInFile(inFile, file.getPath());
+            }
+        }
+    }
+    
 
     protected void findUiLabelMapInFile(String inFile, String filePath) {
         int pos = inFile.indexOf(uiLabelMap);
