908d55a10e8d4040ffd8424be331954ad587730c
Jacques Le Roux <jleroux@apache.org>
2017-03-21 10:55:38 +0000

    Replaces all ™ by ® text symbols Adds svn:ignore for Eclipse
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/ofbiz-new-website@1787934 13f79535-47bb-0310-9956-ffa450edef68

--- template/page/business-users.tpl.php
+++ template/page/business-users.tpl.php
@@ -1,5 +1,5 @@
 <?php //Variable declarations for region templates
-  $head_title = '<title>The Apache OFBiz &trade; Project</title>';
+  $head_title = '<title>The Apache OFBiz&reg; Project</title>';
 ?>
    
   <!-- page content -->
