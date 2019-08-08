981274f34dabef02c333a7a8db555f8b41ab573d
Jacques Le Roux <jleroux@apache.org>
2019-08-08 15:32:08 +0000

    "Applied fix from trunk framework for revision: 1864716" ------------------------------------------------------------------------ r1864716 | jleroux | 2019-08-08 17:28:45 +0200 (jeu. 08 août 2019) | 15 lignes
    Fixed: [FB] Find Security Bugs
    (OFBIZ-9973)
    
    FindBugs is now deprecated and replaced by Spotbugs
    
    Last time I forgot to encode productId as reported by Man Yue Mo from Semmle
    
    This eventually fixes the "Relative path traversal" issue reported by Spotbugs
    by encoding the whole file name.
    
    Nevertheless Spotbugs continues to report the same issue in trunk but not in R16
    I have not ideas why and I see no other possible issue. 
    
    I will backport and check again.
    
    ------------------------------------------------------------------------
    
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/branches/release16.11@1864719 13f79535-47bb-0310-9956-ffa450edef68

--- applications/product/src/main/java/org/apache/ofbiz/product/imagemanagement/FrameImage.java
+++ applications/product/src/main/java/org/apache/ofbiz/product/imagemanagement/FrameImage.java
@@ -30,7 +30,6 @@ import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
@@ -338,16 +337,8 @@ public class FrameImage {
         String imageServerPath = FlexibleStringExpander.expandString(EntityUtilProperties.getPropertyValue("catalog", "image.management.path", delegator), context);
         
         String productId = request.getParameter("productId");
-        String imageName = null;
-        try {
-            imageName = URLEncoder.encode(request.getParameter("imageName"), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            Debug.logError(e, "Error while saving TrackingCodeVisit", module);
-            request.setAttribute("_ERROR_MESSAGE_", e.getMessage());
-            return "error";
-        }
-        
-        
+        String imageName = request.getParameter("imageName");
+
         String dirPath = "/preview/";
         File dir = new File(imageServerPath + dirPath);
         if (!dir.exists()) {
@@ -384,11 +375,12 @@ public class FrameImage {
             request.setAttribute("_ERROR_MESSAGE_", e.getMessage());
             return "error";
         }
+        
         if (UtilValidate.isNotEmpty(imageName)) {
             File file = new File(imageServerPath + "/preview/" +"/previewImage.jpg");
             file.delete();
             // Image Frame
-            BufferedImage bufImg1 = ImageIO.read(new File(imageServerPath + "/" + productId + "/" + imageName).getCanonicalFile());
+            BufferedImage bufImg1 = ImageIO.read(new File(URLEncoder.encode(imageServerPath + "/" + productId + "/" + imageName, "UTF-8")).getCanonicalFile());
             BufferedImage bufImg2 = ImageIO.read(new File(imageServerPath + "/frame/" + frameImageName));
             
             int bufImgType;
